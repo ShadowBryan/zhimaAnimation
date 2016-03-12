ab718d33559aa288b086b6a97e0e8f9ca29faec6
余金 <canye@192.168.2.109>
2016-03-13 00:32:10 +0800

    first

---
+++ zhimaDemo/ProgressView.h
@@ -0,0 +1,13 @@
+//
+//  ProgressView.h
+//  zhimaDemo
+//
+//  Created by 余金 on 16/3/12.
+//  Copyright © 2016年 fengzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgressView : UIView
+
+@end
---
+++ zhimaDemo/ProgressView.m
@@ -0,0 +1,21 @@
+//
+//  ProgressView.m
+//  zhimaDemo
+//
+//  Created by 余金 on 16/3/12.
+//  Copyright © 2016年 fengzhi. All rights reserved.
+//
+
+#import "ProgressView.h"
+
+@implementation ProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
