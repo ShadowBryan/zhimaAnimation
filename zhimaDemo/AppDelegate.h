2c195ba94ecdfb7be413a6a6f4215c461b939c88
余金 <canye321@yeah.net>
2016-03-12 19:47:43 +0800

    Initial Commit

---
+++ zhimaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhimaDemo
+//
+//  Created by 余金 on 16/3/12.
+//  Copyright © 2016年 fengzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhimaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhimaDemo
+//
+//  Created by 余金 on 16/3/12.
+//  Copyright © 2016年 fengzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
